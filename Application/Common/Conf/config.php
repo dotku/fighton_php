8d535bd176f6dcc58fa1b9f38bf8ab781c164aeb
炎藤 <weijingjaylin@gmail.com>
2015-10-11 21:05:33 -0700

    svinsight.com

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'web_title' => '硅谷密探',
+    'DEFAULT_MODULE'     => 'Home', //默认模块
+    'DEFAULT_ACTION'     => 'index', //默认模块
+    'DEFAULT_CONTROLLER'     => 'Index', //默认模块
+    //'MODULE_ALLOW_LIST'    =>    array('Home','Admin','User', 'Pichroom'),
+    //'DEFAULT_MODULE'       =>    'Home',
+    'MULTI_MODULE'          =>  false,
+    'DEFAULT_MODULE'        =>  'Home',
+);
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+    define('APP_PATH','./Application/');
+    define('APP_DEBUG', true);
+    define('RUNTIME_PATH','./Runtime/');
+    define('BIND_MODULE','Home');
+    define('BIND_CONTROLLER','Index');
+    require_once './lib/thinkphp/3.2.3/ThinkPHP.php';
+?>
